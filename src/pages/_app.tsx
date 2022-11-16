32f59a836de0054fe6dd1d0dec601fdedfec41b9
Ula Kaminska <ursz.kam@gmail.com>
2022-11-16 15:42:59 +0100

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { NextPageWithLayout } from "../types/common.types";
 
 import "../styles/globals.css";
+import React from "react";
 
 type Props = AppProps & {
   Component: NextPageWithLayout;
